92a58ef21f9803e7765fffafcd9441d8d84e43d0
leandramusto@gmail.com <leandramusto@gmail.com>
2024-03-05 18:52:26 +0100

    repetition wrong

--- snack 1/assets/js/script.js
+++ snack 1/assets/js/script.js
@@ -1,31 +1,28 @@
 // Create an array of 10 cars
 // Each car object will have the following properties: make, model, and power supply
 let cars = [
-  { brand: "Ford", model: "Focus", fuel: "petrol", powerSupply: "gasoline" },
-  { brand: "Toyota", model: "Corolla", fuel: "diesel", powerSupply: "diesel" },
-  { brand: "Volkswagen", model: "Golf", fuel: "lpg", powerSupply: "LPG" },
+  { brand: "Ford", model: "Focus", powerSupply: "gasoline" },
+  { brand: "Toyota", model: "Corolla", powerSupply: "diesel" },
+  { brand: "Volkswagen", model: "Golf", powerSupply: "LPG" },
   {
     brand: "Tesla",
     model: "Model 3",
-    fuel: "electric",
     powerSupply: "electric",
   },
-  { brand: "Fiat", model: "500", fuel: "methane", powerSupply: "methane" },
-  { brand: "Renault", model: "Clio", fuel: "petrol", powerSupply: "gasoline" },
-  { brand: "BMW", model: "X5", fuel: "diesel", powerSupply: "diesel" },
+  { brand: "Fiat", model: "500", powerSupply: "methane" },
+  { brand: "Renault", model: "Clio", powerSupply: "gasoline" },
+  { brand: "BMW", model: "X5", powerSupply: "diesel" },
   {
     brand: "Hyundai",
     model: "Kona",
-    fuel: "electric",
     powerSupply: "electric",
   },
   {
     brand: "Mercedes",
     model: "A-Class",
-    fuel: "petrol",
     powerSupply: "gasoline",
   },
-  { brand: "Audi", model: "Q7", fuel: "diesel", powerSupply: "diesel" },
+  { brand: "Audi", model: "Q7", powerSupply: "diesel" },
 ];
 
 // Divide cars into 3 separate arrays: in the first array only petrol cars, in the second only diesel cars, in the third the rest of cars.
